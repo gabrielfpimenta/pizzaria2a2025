05767fc236350a468d5a490e2e9b873eaeb03f5e
gabrielfpimenta <rm93995@estudante.fieb.edu.br>
2025-09-17 09:58:06 -0300

    novo index

--- src/pages/produtos/index.jsx
+++ src/pages/produtos/index.jsx
@@ -1,52 +1,33 @@
 import {useState, useEffect} from 'react'
 import axios from 'axios'
-
-
+ 
 const Produtos = () => {
-
+ 
     const [dados, setDados] = useState([]);
-
-    const pegarPizzas = async () => {
-
-        await axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto",
-            {withCredentials: true}
-        )
-
-        .then((response) => setDados(response.data))
+ 
+    useEffect(()=>{  
+        axios.get("http://172.19.0.49/pizzariaoficial/api/v1/produto")
+        .then((response) => {
+            console.log(response.data.data)
+            setDados(response.data.data)
+        })
         .catch((error) => console.log(error))
-
-    }
-
-    useEffect(()=>{
-        pegarPizzas();
     }, [])
-
-    useEffect(()=>{
-        console.log(dados);
-    }, [])
-
-    // Objetos de produtos da lista
-    const pizzas = [
-    'Pizza de Muçarela',
-    'Pizza de Chumbo',
-    'Pizza de Calabreso',
-    'Língua Portuguesa',
-    'Pizza de Franguinha'
-]
-
-// Iteração da lista de pizzas
-
-const listaPizzas = pizzas.map(pizza => <li>{pizza}</li>);
-
+ 
+    // Iteração da lista de pizzas
+    const listaPizzas = dados.map(pizza =>
+        <li key={pizza.id}>{pizza.nome}</li>)
+ 
     return (
         <div>
-            <h3> Listagem de Produtos</h3>
-
+            <h3>Listagem de Produtos</h3>
+ 
             <ul>
                 {listaPizzas}
             </ul>
         </div>
     )
 }
-
+ 
 export default Produtos
+ 
